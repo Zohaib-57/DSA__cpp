dda6854de3796fd8cb97f274fd650c545d132d34
Your Name <zohaibabbas8557@gmail.com>
2025-11-03 13:15:52 +0500

    patterns practise for DSA

---
+++ Day_4/square.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int side =4;
+    // for(int i=0;i<+side;i++){
+    //     for(int j=0;j<+side;j++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // int n =3 ;
+
+    // int num = 1;
+
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<n;j++){
+    //         cout<<num<<" ";
+    //         num++;
+    //     }
+    //     cout<<endl;
+    // }
+
+    char ch = 'A';
+    int n =3 ;
+    for(int i = 0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<ch<<" ";
+            ch++;
+        }
+        cout<<endl;
+    }
+}
+
---
+++ Day_4/triangle.cpp
@@ -0,0 +1,122 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n = 4;
+
+// Star Triangle
+// for(int i=1; i<=n; i++){
+//     for(int j=1; j<=i; j++){
+//         cout<<"* ";
+//     }
+//     cout<<endl;
+// }
+//     return 0;
+
+
+// Number Triangle
+// for(int i=0; i<n; i++){
+//     for(int j=0; j<i+1; j++){
+//         cout<<i+1;
+//     }
+//     cout<<endl;
+// }
+//     return 0;
+
+
+// Alpahabet Triangle
+    // char ch = 'A';
+    // for (int i = 0; i < n; i++) {
+    //     for (int j = 0; j < i + 1; j++) {
+    //         cout << ch;
+    //     }
+    //     cout << endl;
+    //     ch++; 
+    // }
+    // return 0;
+
+
+// number traingle pattern
+    // for (int i=0; i<n; i++) {
+    //     for (int j=1; j<=i+1; j++) {
+    //         cout<<j<<" ";
+    //     }
+    //     cout << endl;
+    // }
+    // return 0;
+
+
+
+// reverse triangle number pattern
+    // for(int i=0;i<n;i++){
+    //     for(int j=i;j>0;j--){
+    //         cout<<j<<" ";        
+    //     }
+    //     cout<<endl;
+    // }
+//     return 0;
+
+//Reverse triangle alphabet pattern
+    // for(int i = 0; i < n; i++) {
+    //     for(int j = i; j >= 0; j--) {
+    //         char ch = 'A' + j;
+    //         cout << ch << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // return 0;
+
+
+//Floyd's Triangle number version
+    // int num = 1;
+    // for(int i=0;i<n;i++){
+    //     for(int j=i+1;j>0;j--){
+    //         cout<<num<<" ";
+    //         num++;
+    //     }
+    //     cout<<endl;
+    // }
+    // return 0;
+
+
+//Floyd's Triangle alphabet version
+    // char ch = 'A';
+    // for(int i=0;i<n;i++){
+    //     for(int j=i+1;j>0;j--){
+    //         cout<<ch<<" ";
+    //         ch++;
+    //     }
+    //     cout<<endl;
+    // }
+    // return 0;
+
+
+    //Inverted number triangle pattern
+    // for (int i = 0; i < n; i++) {
+    //     for (int j = 0; j < i; j++) {
+    //         cout << " ";
+    //     }
+    //     for (int j = 0; j < n - i; j++) {
+    //         cout << i + 1;
+    //     }
+    //     cout << endl;
+    // }
+
+    // return 0;
+
+
+    //Pyramid number pattern
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<n-i-1;j++){
+    //         cout<<" ";
+    //     }
+    //     for(int j=1;j<=i+1;j++){
+    //         cout<<j;
+    //     }
+    //     for(int j=i; j>0; j--){
+    //         cout<<j;
+    //     }
+    //     cout<<endl;
+    // }
+    // return 0;
+}
